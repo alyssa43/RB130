7491d1a3c2b16f6994ec771f21386e9df4abe776
Alyssa Easter <AlyssaStarley@gmail.com>
2024-08-21 15:55:38 -0700

    Completed TodoList#select assignment

---
+++ lesson_1/to_do_list_select.rb
@@ -0,0 +1,150 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+
+  def ==(otherTodo)
+    title == otherTodo.title &&
+      description == otherTodo.description &&
+      done == otherTodo.done
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(item)
+    if valid_item?(item)
+      @todos << item
+    else
+      raise TypeError.new("Can only add Todo objects")
+    end
+  end
+
+  def <<(item)
+    add(item)
+  end
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def to_a
+    @todos.clone
+  end
+
+  def done?
+    @todos.all? { |item| item.done? }
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def done!
+    @todos.each do |todo|
+      todo.done!
+    end
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    @todos.delete(item_at(index))
+  end
+
+  def to_s
+    text = "--- #{title} ---\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def each
+    @todos.each do |todo|
+      yield(todo)
+    end
+    self
+  end
+
+  def select
+    selected = TodoList.new(title)
+
+    each do |todo|
+      status = yield(todo)
+      selected << todo if status
+    end
+
+    selected
+  end
+
+  private
+
+  def valid_item?(item)
+    item.class == Todo
+  end
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+todo1.done!
+
+results = list.select { |todo| todo.done? }    # you need to implement this method
+
+puts results.inspect
